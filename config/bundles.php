5202ce4faece6a069ad7d489543cfc1dd4874db7
Dev-Int <devint.creation@gmail.com>
2024-04-14 01:42:48 +0200

    fix(chore): 49 upgrade to symfony 7.0.*
    - update recipes

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
