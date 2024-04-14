7891a977acdb65b25d6c245ee0c4f668f43f5a12
Quétier Laurent <Dev-Int@users.noreply.github.com>
2024-04-14 05:31:00 +0200

    feat(Admin): improve flash boxes (#59)

---
+++ assets/controllers/toast_controller.js
@@ -0,0 +1,10 @@
+import {Controller} from '@hotwired/stimulus';
+
+export default class extends Controller {
+    connect()
+    {
+        const flashes = this.element;
+
+        setTimeout(() => {flashes.classList.add('hidden')}, 2000);
+    }
+}
