65c8f8e1db5e331a3bf8b15deca6bb1c6f86ddfd
Dev-Int <devint.creation@gmail.com>
2025-12-22 12:33:35 +0100

    refactor(Admin): 167 migrate service configuration from YAML to PHP

---
+++ src/Admin/Frameworks/config/services.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Tests package.
+ *
+ * (c) Dev-Int Création <info@developpement-interessant.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $configurator): void {
+    $services = $configurator->services();
+
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->private()
+    ;
+
+    $services->load(namespace: 'Admin\\', resource: __DIR__ . '/../../../Admin')
+        ->exclude(__DIR__ . '/../../../Admin/{Frameworks,Entities,Tests}')
+        ->exclude(__DIR__ . '/../../../Admin/Adapters/Gateway/ORM/Entity')
+    ;
+
+    $services->load(
+        namespace: 'Admin\Entities\Repository\\',
+        resource: __DIR__ . '/../../../Admin/Entities/Repository'
+    );
+
+    $services->alias(
+        id: 'Admin\Contracts\Services\Provider\Article\ArticleProvider',
+        referencedId: 'Admin\Adapters\Gateway\Contracts\Provider\Article\ArticleProvider'
+    )
+        ->public()
+    ;
+};
